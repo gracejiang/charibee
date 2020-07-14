2cb29a79839d1f33fcaf02938698df132decc112
Grace Jiang <gracj@fb.com>
2020-07-13 20:12:50 -0700

    ui: added fragments and fragment manager

--- app/src/main/java/com/example/service/MainActivity.java
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.service;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.service.Fragments.DiscoverFragment;
+import com.example.service.Fragments.HomeFragment;
+import com.example.service.Fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // fragment manager
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // bottom nav
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.menu_discover:
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.menu_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    default:
+                        fragment = null;
+                }
+                fragmentManager.beginTransaction().replace(R.id.main_frame_layout, fragment).commit();
+                return true;
+            }
+        });
+
+        // default bottom nav selection
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
     }
 }
