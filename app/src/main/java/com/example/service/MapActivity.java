952a371cb0574a62044012f53646f4386fc482ec
Grace Jiang <gracj@fb.com>
2020-07-25 17:51:25 -0700

    map can grab user's current location

--- app/src/main/java/com/example/service/MapActivity.java
+++ app/src/main/java/com/example/service/MapActivity.java
@@ -7,24 +7,34 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends AppCompatActivity {
 
     public static final String TAG = "MapActivity";
 
-    public static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    public static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    public static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
 
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +42,82 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         getLocationPermission();
+        if (mLocationPermissionsGranted) {
+            initMap();
+            getDeviceLocation();
+        }
 
     }
 
+    // initialize map fragment
     private void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_map);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                Toast.makeText(MapActivity.this, "map is ready", Toast.LENGTH_SHORT).show();
-                Log.i(TAG, "map is ready");
+                // Log.i(TAG, "initMap: map is ready");
                 mMap = googleMap;
             }
         });
 
     }
 
+    // get devices location
+    private void getDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mLocationPermissionsGranted) {
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Log.i(TAG, "onComplete: found location");
+                            Location currLocation = (Location) task.getResult();
+                            LatLng currLocationLatLng = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());
+
+
+                            // display current location
+                            moveCamera(currLocationLatLng, DEFAULT_ZOOM);
+
+                            // checks permissions (required for setMyLocationEnabled function)
+                            if (ActivityCompat.checkSelfPermission(MapActivity.this, FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapActivity.this, COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                return;
+                            }
+                            mMap.setMyLocationEnabled(true);
+
+                        } else {
+                            Log.i(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException" + e.getMessage());
+        }
+    }
+
+    // move camera to current lat/long
+    private void moveCamera(LatLng latLng, float zoom) {
+        // Log.i(TAG, "moving camera to lat: " + latLng.latitude + ", lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    // if all the permissions are granted
     private void getLocationPermission() {
         String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED &&
-            ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION)  == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionsGranted = true;
         } else {
+            // will invoke onRequestPermissionsResult
             ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 
+    // set mLocationPermissionsGranted to correct value
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         mLocationPermissionsGranted = false;
@@ -71,10 +129,7 @@ public class MapActivity extends AppCompatActivity {
                             return;
                         }
                     }
-
                     mLocationPermissionsGranted = true;
-                    // initialize our map
-                    initMap();
                 }
             }
         }
