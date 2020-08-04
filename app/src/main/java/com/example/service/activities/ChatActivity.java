3090ab402f5374d5c4ca5d7db1eb2e05258dc953
Grace Jiang <gracj@fb.com>
2020-08-04 06:09:45 -0700

    finished raw chat feature

--- app/src/main/java/com/example/service/activities/ChatActivity.java
+++ app/src/main/java/com/example/service/activities/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.service.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -8,12 +9,21 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.service.R;
+import com.example.service.functions.ChatAdapter;
 import com.example.service.models.Message;
+import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -21,10 +31,18 @@ public class ChatActivity extends AppCompatActivity {
 
     static final String USER_ID_KEY = "userId";
     static final String BODY_KEY = "body";
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
 
     // ui views
     EditText etMessage;
     ImageButton btnSend;
+
+    // data
+    RecyclerView rvChat;
+    ArrayList<Message> mMessages;
+    ChatAdapter mAdapter;
+    boolean mFirstLoad; // Keep track of initial load to scroll to the bottom of the ListView
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +57,17 @@ public class ChatActivity extends AppCompatActivity {
         // Find the text field and button
         etMessage = (EditText) findViewById(R.id.etMessage);
         btnSend = (ImageButton) findViewById(R.id.btSend);
+        rvChat = (RecyclerView) findViewById(R.id.rvChat);
+        mMessages = new ArrayList<>();
+        mFirstLoad = true;
+        final String userId = ParseUser.getCurrentUser().getObjectId();
+        mAdapter = new ChatAdapter(ChatActivity.this, userId, mMessages);
+        rvChat.setAdapter(mAdapter);
+
+        // associate the LayoutManager with the RecylcerView
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        linearLayoutManager.setReverseLayout(true); // orders msgs from oldest to newest
+        rvChat.setLayoutManager(linearLayoutManager);
 
         // When send button is clicked, create message object on Parse
         btnSend.setOnClickListener(new View.OnClickListener() {
@@ -54,9 +83,10 @@ public class ChatActivity extends AppCompatActivity {
                 message.saveInBackground(new SaveCallback() {
                     @Override
                     public void done(ParseException e) {
-                        if(e == null) {
+                        if (e == null) {
                             Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
                                     Toast.LENGTH_SHORT).show();
+                            refreshMessages();
                         } else {
                             Log.e(TAG, "Failed to save message", e);
                         }
@@ -65,5 +95,62 @@ public class ChatActivity extends AppCompatActivity {
                 etMessage.setText(null);
             }
         });
+    }
+
+    // Query messages from Parse so we can load them into the chat adapter
+    void refreshMessages() {
+        // Construct query to execute
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+
+        // get the latest 50 messages, order will show up newest to oldest of this group
+        query.orderByDescending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    mAdapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    if (mFirstLoad) {
+                        rvChat.scrollToPosition(0);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.e("message", "Error Loading Messages" + e);
+                }
+            }
+        });
+    }
+
+    // MORE EFFICIENT QUERYING (every 3 seconds instead of constant)
+
+    // Create a handler which can run code periodically
+    static final long POLL_INTERVAL = TimeUnit.SECONDS.toMillis(3);
+    Handler myHandler = new android.os.Handler();
+    Runnable mRefreshMessagesRunnable = new Runnable() {
+        @Override
+        public void run() {
+            refreshMessages();
+            myHandler.postDelayed(this, POLL_INTERVAL);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Only start checking for new messages when the app becomes active in foreground
+        myHandler.postDelayed(mRefreshMessagesRunnable, POLL_INTERVAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Stop background task from refreshing messages, to avoid unnecessary traffic & battery drain
+        myHandler.removeCallbacksAndMessages(null);
+        super.onPause();
     }
 }
